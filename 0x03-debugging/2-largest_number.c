398a32630d2d1036a8e542f32f716f4dc50e7613
Akinde1 <Akinde1>
2022-03-18 18:19:10 -0700

    commit

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -3,11 +3,9 @@
 /**
  * largest_number - returns the largest of 3 numbers
  * @a: first integer
- 
-
-* @b: second integer
- *@c: third integer
- *Return: largest number
+ * @b: second integer
+ * @c: third integer
+ * Return: largest number
  */
 
 int largest_number(int a, int b, int c)
@@ -15,7 +13,7 @@ int largest_number(int a, int b, int c)
 int largest;
 
 if (a >= b && a >= c)
-}
+{
 largest = a;
 }
 else if (b >= a && b >= c)
