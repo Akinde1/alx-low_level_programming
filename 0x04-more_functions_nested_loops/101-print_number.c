e9d55a8f0e03bd7715bfd3914aac7ef0d94ed360
Akinde Titilayo Tunmise <93851293+Akinde1@users.noreply.github.com>
2022-03-19 02:22:48 +0000

    Update 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,23 +1,22 @@
 #include "main.h"
+
 /**
- * print_number - prints number
- * @n: integer to be printed
+ * print_number - prints an integer
+ * @n: tracked var
  */
+
 void print_number(int n)
 {
-	unsigned int j;
+	unsigned int x = n;
 
 	if (n < 0)
 	{
-		j = -n;
-		_putchar('-');
+		_putchar(45);
+		x = -x;
 	}
-	else
+	if ((x / 10) > 0)
 	{
-		j = n;
+		print_number(x / 10);
 	}
-	if (j / 10)
-	{
-		print_number(j / 10);
-	}
-	_putchar((j % 10) + '0');
+	_putchar((x % 10) + 48);
+}
