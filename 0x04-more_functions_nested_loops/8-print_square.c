b32c1193444417d9e624fa8f16faaf10aa8aada8
Akinde Titilayo Tunmise <93851293+Akinde1@users.noreply.github.com>
2022-03-19 02:34:02 +0000

    Update 8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,27 +1,27 @@
 #include "main.h"
+
 /**
- * print_square -prints a square
- * @size: print size
- * Return: Always 0.
+ * print_square - prints a square
+ * @size: the size of the square
  */
+
 void print_square(int size)
 {
-	int i;
-	int k;
+	int x, y;
 
 	if (size > 0)
 	{
-		for (i = 0; i < size; i++)
+		for (x = 0; x < size; x++)
 		{
-			for (k = 0; k < size; k++)
+			for (y = 0; y < size; y++)
 			{
-				_putchar (35);
+				_putchar(35);
 			}
-			_putchar ('\n');
+			_putchar(10);
 		}
 	}
 	else
 	{
-		_putchar ('\n');
+		_putchar(10);
 	}
 }
